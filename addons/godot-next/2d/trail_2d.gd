b52e8e90dcb4f6e61f36ec68f47dfeee380f7386
Will Nations <willnationsdev@gmail.com>
2019-08-21 15:30:00 -0500

    Add emptiness signals to Trail2D.

--- addons/godot-next/2d/trail_2d.gd
+++ addons/godot-next/2d/trail_2d.gd
@@ -23,6 +23,11 @@ class_name Trail2D, "../icons/icon_trail_2d.svg"
 
 ##### SIGNALS #####
 
+# If true, notifies that points have all been removed. Else, points are now present.
+signal trail_zero_changed(p_no_points)
+# If true, all points are equal, i.e. the trail has vanished. Else, the trail has appeared again.
+signal trail_empty_changed(p_is_empty)
+
 ##### CONSTANTS #####
 
 enum Persistance {
@@ -40,6 +45,10 @@ enum PersistWhen {
 
 # The target node to track
 var target: Node2D setget set_target
+# Cache instances when there are no points
+var _zero_points: bool = true
+# Cache instances when points are all equal
+var _empty: bool = true
 
 # The NodePath to the target
 export var target_path: NodePath = @".." setget set_target_path
@@ -53,12 +62,14 @@ export(int, "On Movement", "Custom") var persistance_condition: int = PersistWhe
 export var degen_rate: int = 1
 # If true, automatically set z_index to be one less than the 'target'
 export var auto_z_index: bool = true
-# If true, will automatically setup a gradient for a gradually transparent trail
+# If true, will automatically setup a gradient for a gradually transparent trail.
 export var auto_alpha_gradient: bool = true
+# If true, spend time determining when emptiness occurs to trigger signals. Else, do not waste processing time.
+export var track_emptiness: bool = true
 
 ##### NOTIFICATIONS #####
 
-func _init():
+func _init() -> void:
 	set_as_toplevel(true)
 	global_position = Vector2()
 	global_rotation = 0
@@ -69,7 +80,7 @@ func _init():
 		gradient.set_color(0, first)
 		gradient.set_color(1, default_color)
 
-func _notification(p_what: int):
+func _notification(p_what: int) -> void:
 	match p_what:
 		NOTIFICATION_PARENTED:
 			self.target_path = target_path
@@ -80,7 +91,7 @@ func _notification(p_what: int):
 			self.trail_length = 0
 
 #warning-ignore:unused_argument
-func _process(delta: float):
+func _process(delta: float) -> void:
 	if target:
 		match persistance:
 			Persistance.OFF:
@@ -97,16 +108,41 @@ func _process(delta: float):
 						if not get_point_count() or moved:
 							add_point(target.global_position)
 						else:
-							#warning-ignore:unused_variable
-							for i in range(degen_rate):
+							for _i in range(degen_rate):
 								remove_point(0)
 					PersistWhen.CUSTOM:
 						if _should_grow():
 							add_point(target.global_position)
 						if _should_shrink():
-							#warning-ignore:unused_variable
-							for i in range(degen_rate):
+							for _i in range(degen_rate):
 								remove_point(0)
+		_update_emptiness()
+
+func _update_emptiness() -> void:
+	if not track_emptiness:
+		return
+	
+	# handle case where points really are empty / become empty
+	if _zero_points:
+		if get_point_count():
+			_zero_points = false
+			emit_signal("trail_zero_changed", _zero_points)
+	else:
+		if not get_point_count():
+			_zero_points = true
+			emit_signal("trail_zero_changed", _zero_points)
+	
+	# handle case where points exist, but are all equal, i.e. no trail
+	if get_point_count():
+		var first = get_point_position(0)
+		var trail_exists = false
+		for i in get_point_count():
+			if first != get_point_position(i):
+				trail_exists = true
+				break
+		if _empty != trail_exists:
+			_empty = not trail_exists
+			emit_signal("trail_empty_changed", _empty)
 
 ##### OVERRIDES #####
 
@@ -120,21 +156,21 @@ func _should_shrink() -> bool:
 
 ##### PUBLIC METHODS #####
 
-func erase_trail():
-	for i in range(get_point_count()):
+func erase_trail() -> void:
+	for _i in range(get_point_count()):
 		remove_point(0)
 
 ##### PRIVATE METHODS #####
 
 ##### SETTERS AND GETTERS #####
 
-func set_target(p_value: Node2D):
+func set_target(p_value: Node2D) -> void:
 	if p_value:
 		if get_path_to(p_value) != target_path:
 			target_path = get_path_to(p_value)
 	else:
 		target_path = @""
 
-func set_target_path(p_value: NodePath):
+func set_target_path(p_value: NodePath) -> void:
 	target_path = p_value
 	target = get_node(p_value) as Node2D if has_node(p_value) else null
